1873ad415cf0480ad9e57ff38f76974fe21295b5
Dennis Ranke <dennis.ranke@gmail.com>
2014-05-01 16:05:39 +0200

    implement key input

--- game.rs
+++ game.rs
@@ -5,10 +5,14 @@ mod gba;
 #[start]
 pub fn main(_: int, _: **u8) -> int {
     let mut col = 0u16;
+    let mut key_state = gba::KeyState::new();
     gba::hw::write_dispcnt(0);
     loop {
+        key_state.update();
+        if key_state.is_pressed(gba::KeyUp) { col += 1 }
+        if key_state.is_pressed(gba::KeyDown) { col -= 1 }
+        if key_state.is_triggered(gba::KeyA) { col += 0x2000 }
         gba::hw::write_pal(0, col);
-        col += 1;
         gba::wait_vblank();
     }
 }
--- gba.rs
+++ gba.rs
@@ -30,11 +30,42 @@ pub mod hw {
         unsafe { write16(0x4000004, value) }
     }
 
+    pub fn read_keyinput() -> u16 {
+        unsafe { read16(0x4000130) }
+    }
+
     pub fn write_pal(index: uint, col: u16) {
         if index < 512 {
             unsafe { write16(0x5000000u32 + (index * 2) as u32, col) }
         }
     }
+}
+
+pub struct KeyState { state: u32 }
+pub enum Key {
+    KeyA = 1u16,
+    KeyB = 2u16,
+    KeySelect = 4u16,
+    KeyStart = 8u16,
+    KeyRight = 16u16,
+    KeyLeft = 32u16,
+    KeyUp = 64u16,
+    KeyDown = 128u16,
+    KeyR = 256u16,
+    KeyL = 512u16
+}
+
+impl KeyState {
+    pub fn new() -> KeyState { KeyState { state: 0 } }
+    pub fn update(&mut self) {
+        let pressed = hw::read_keyinput() ^ 0xffffu16;
+        let triggered = pressed & !self.get_pressed();
+        self.state = (pressed as u32) | ((triggered as u32) << 16);
+    }
+    fn get_pressed(&self) -> u16 { self.state as u16 }
+    fn get_triggered(&self) -> u16 { (self.state >> 16) as u16 }
+    pub fn is_pressed(&self, key: Key) -> bool { self.get_pressed() & (key as u16) != 0 }
+    pub fn is_triggered(&self, key: Key) -> bool { self.get_triggered() & (key as u16) != 0 }
 }
 
 pub fn wait_vblank() {
